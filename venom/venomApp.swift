433eebe9d102d0a29c53ea6942fd51013d4345c7
Mitch Stark <mitchstark10@gmail.com>
2024-03-17 09:16:31 -0500

    Initial Commit

---
+++ venom/venomApp.swift
@@ -0,0 +1,17 @@
+//
+//  venomApp.swift
+//  venom
+//
+//  Created by Mitch Stark on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct venomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
