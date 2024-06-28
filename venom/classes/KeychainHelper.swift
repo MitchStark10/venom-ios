a1fd6169d9ba49287e88819743d31a1b55f32e03
Mitch Stark <mitchstark10@gmail.com>
2024-06-28 15:20:15 -0500

    API token stored in keychain!

---
+++ venom/classes/KeychainHelper.swift
@@ -0,0 +1,8 @@
+//
+//  KeychainHelper.swift
+//  venom
+//
+//  Created by Mitch Stark on 6/28/24.
+//
+
+import Foundation
---
+++ venom/lib/initializeSignInStatus.swift
@@ -0,0 +1,8 @@
+//
+//  initializeSignInStatus.swift
+//  venom
+//
+//  Created by Mitch Stark on 6/28/24.
+//
+
+import Foundation
